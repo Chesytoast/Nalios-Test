2843762874187598d1589e135e707d7e224d6ab4
Chesytoast <mattiscourtin@gmail.com>
2025-04-14 16:18:30 +0200

    ex5 bonus

--- ex5/main.py
+++ ex5/main.py
@@ -2,13 +2,22 @@ def main():
     numb = [1,4,6,123,2,3,3,65,3]
     myRes = mySum(numb)
     res = sum(numb)
-    print(f"res: {res} \n myres: {myRes}")
+    print(f"res: {res} \nmyres: {myRes}")
+
+    recRes = recursiveSum(numb, 0)
+    print(f"recrusiver res: {recRes}")
+
 
 def mySum(numb):
     total = 0
     for n in numb:
         total += n
     return total
+
+def recursiveSum(numb, i):
+    if i >= len(numb):
+        return 0
+    return numb[i] + recursiveSum(numb, i + 1)
 
 if __name__ == "__main__":
     main()
